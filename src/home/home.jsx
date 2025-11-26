d55465328e3c3e220b33a0d87ec4d1af0abdfbfc
Julian <juliansepulveda0206@gmail.com>
2025-11-26 17:24:56 -0500

    tercer demo

--- src/home/home.jsx
+++ src/home/home.jsx
@@ -16,8 +16,22 @@ const Home = () => {
           <img src={hoja2} alt="..." className='hoja-fondo2'/>
           <img src={hoja1} alt="..." className='hoja-fondo3'/>
           <img src={hoja2} alt="..." className='hoja-fondo4'/>
+          <img src={hoja1} alt="..." className='hoja-fondo5'/>
+          <img src={hoja2} alt="..." className='hoja-fondo6'/>
+          <img src={hoja1} alt="..." className='hoja-fondo7'/>
+          <img src={hoja2} alt="..." className='hoja-fondo8'/>
+          <img src={hoja1} alt="..." className='hoja-fondo9'/>
+          <img src={hoja2} alt="..." className='hoja-fondo10'/>
+          <img src={hoja1} alt="..." className='hoja-fondo11'/>
+          <img src={hoja2} alt="..." className='hoja-fondo12'/>
+          <img src={hoja1} alt="..." className='hoja-fondo13'/> 
           <img src={logo} alt="..." className='principal-barra-imagen'/>
         </div>
+        <div className='principal-video-presentación'>
+        </div>
+        <div className='principal-nav'>
+        </div>
+        <div className='principal-nav-linea'></div>
       </div>
     </>
   )
